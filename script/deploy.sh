ae4d97aea003f9f125b63ee475150f2a3502b5f1
showwin <showwin_kmc@yahoo.co.jp>
2015-04-28 21:12:37 +0900

    change deploy branch for auto-deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,6 @@
 
 curl -i -H "Accept: application/json" \
  -H "Content-Type:application/json" \
- -X POST --data '{"name":"showwin", "branch":"production"}' \
+ -X POST --data '{"name":"showwin", "branch":"master"}' \
  http://deploy.fc2play.com
 echo 'throwing Webhook...'
