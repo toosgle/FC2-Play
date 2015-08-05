e7fe02b95aa6c1a006e55065da35db4d074e42e0
showwin <showwin_kmc@yahoo.co.jp>
2015-08-05 21:57:18 +0900

    add missed file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'coveralls'
 Coveralls.wear!('rails')
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
 require 'rspec/autorun'
 require 'simplecov'
 require 'shoulda/matchers'
