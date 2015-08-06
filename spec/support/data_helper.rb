1fb32ee9d5052ecc3720fad618721bda6ddef2ea
showwin <showwin_kmc@yahoo.co.jp>
2015-08-06 10:00:25 +0900

    fix spec

--- spec/support/data_helper.rb
+++ spec/support/data_helper.rb
@@ -2,14 +2,20 @@ module DataHelper
   def create_base_data
     create(:user)
     10.times do |i|
-      create(:video, id: i)
-      create(:new_arrival, video_id: i)
+      next_id = last_video_id + 1
+      create(:video, id: i + next_id)
+      create(:new_arrival, video_id: i + next_id)
     end
     50.times do |i|
-      create(:video, id: i + 100)
-      create(:weekly_rank, video_id: i + 100)
-      create(:video, id: i + 150)
-      create(:monthly_rank, video_id: i + 150)
+      next_id = last_video_id + 1
+      create(:video, id: i + next_id)
+      create(:weekly_rank, video_id: i + next_id)
+      create(:video, id: i + next_id + 50)
+      create(:monthly_rank, video_id: i + next_id + 50)
     end
+  end
+
+  def last_video_id
+    Video.last.present? ? Video.last.id : 0
   end
 end
